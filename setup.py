3b5b58f1a096357beb5017b9272ad6689f83f287
itaysason <itaysason@mail.tau.ac.il>
2022-02-14 16:17:52 +0200

    Changed name to kltpicker2 in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='kltpicker',
+setup(name='kltpicker2',
       version='2.0.1',
       description='KLT picker',
       long_description=long_description,
